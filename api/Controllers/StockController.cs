1bf6a0280597d15d1c5f678027a306abbb818d48
TheQuang <thequang012004@gmail.com>
2025-03-01 12:46:28 +0700

    additional async/await

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -85,12 +85,12 @@ namespace api.Controllers
 
         [HttpDelete]
         [Route("{id}")]
-        public IActionResult Delete([FromRoute] int id)
+        public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
             if(stockModel == null) return NotFound();
             _context.Stocks.Remove(stockModel);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             
             //NoContent() is the good thumbs up (green one) for succeeded deleting
             return NoContent();
