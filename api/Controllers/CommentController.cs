a7e6bd6db34e6317630ecd44e1d1fb4d195b08a3
TheQuang <thequang012004@gmail.com>
2025-03-02 17:37:22 +0700

    1-To-Many CREATE (Post)

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Security;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Comment;
+using api.Dtos.Stock;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -14,12 +16,13 @@ namespace api.Controllers
     [ApiController]
     public class CommentController: ControllerBase
     {
-        private readonly ApplicationDBContext _context;
+        // private readonly ApplicationDBContext _context;
         private readonly ICommentRepository _commentRepo;
-        public CommentController(ApplicationDBContext context ,ICommentRepository commentRepo)
+        private readonly IStockRepository _stockRepo;
+        public CommentController(ICommentRepository commentRepo, IStockRepository stockRepo)
         {
-            _context = context;
             _commentRepo = commentRepo;
+            _stockRepo = stockRepo;
         }
         
         [HttpGet]
@@ -35,6 +38,17 @@ namespace api.Controllers
             var comment = await _commentRepo.GetByIdAsync(id);
             if(comment == null) return NotFound();
             return Ok(comment.ToCommentDto());
+        }
+        [HttpPost("{stockId}")]
+        public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto)
+        {
+            if(!await _stockRepo.StockExists(stockId))
+            {
+                return BadRequest("Stock does not exist");
+            }
+            var commentModel = commentDto.ToCommentFromCreate(stockId);
+            await _commentRepo.CreateAsync(commentModel);
+            return CreatedAtAction(nameof(GetById), new {id = commentModel}, commentModel.ToCommentDto());
         }
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e099e929dcf0b7eb550dd457539a1ba32cd2e43e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e301714633b10712e3fa7c8af65d0802f7ffef93")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
