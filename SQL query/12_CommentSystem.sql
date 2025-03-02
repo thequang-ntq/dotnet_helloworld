e099e929dcf0b7eb550dd457539a1ba32cd2e43e
TheQuang <thequang012004@gmail.com>
2025-03-02 15:44:01 +0700

    Comment system

---
+++ SQL query/12_CommentSystem.sql
@@ -0,0 +1,3 @@
+USE helloworld_dotnet;
+INSERT INTO comments(Title, Content, CreatedOn, StockId)
+VALUES	("Text Comment", "Text Content", "1990-02-02", 1);
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8da3c4f2110f18228dea790c085f4b3ba24ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f6c4a8472574f9b3d58a8d1785cd0db5617434")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
