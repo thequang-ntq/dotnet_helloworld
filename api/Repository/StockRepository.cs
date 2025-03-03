e76e33f2bdd0e1349c20609d33e941846bf6e98e
TheQuang <thequang012004@gmail.com>
2025-03-03 10:50:50 +0700

    Pagination

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -60,7 +60,9 @@ namespace api.Repository
                     stocks.OrderBy(s => s.Symbol);
                 }
             }
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
